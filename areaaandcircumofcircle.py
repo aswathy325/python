b9256b9dbdc529157f5eaed234b80b727929eda6
aswathy325 <aswathyabhiii325@gmail.com>
2025-03-07 15:23:08 +0530

    Create areaaandcircumofcircle.py

---
+++ areaaandcircumofcircle.py
@@ -0,0 +1,8 @@
+radius=float(input())
+pi=3.1416
+circumference=2*pi*radius
+area=pi*radius*radius
+#circum=round(circumference,2)
+#areas=round(area,2)
+print("The circumference of the circle is: {:.2f}".format(circumference))
+print("The area of the circle is: {:.2f}".format(area))
